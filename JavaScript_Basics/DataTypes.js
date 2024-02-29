ef4b27a3b6492a5a0b3ffe2dd5ed00637e70d2be
Mohd Sohaib <160093489+sohaibazmi101@users.noreply.github.com>
2024-02-29 11:16:40 +0000

    DataTypes

---
+++ JavaScript_Basics/DataTypes.js
@@ -0,0 +1,37 @@
+/*
+A large of data type we will use in web and software development journey
+*/
+
+
+//  boolean tells true or fales
+//  int x = 10; its an iteger value
+
+// typeof is used find the type of datatypes
+
+var x = 100;
+let name = "Sohaib Azmi";
+var Boolean, isActive = true;
+
+console.table([x, name, isActive]);
+
+console.table([typeof x, typeof name, typeof isActive]);    // It is used to print the table form of multiple variable
+
+/*
+
+Possible outputs are 
+
+┌─────────┬───────────────┐
+│ (index) │ Values        │
+├─────────┼───────────────┤
+│ 0       │ 100           │
+│ 1       │ 'Sohaib Azmi' │
+│ 2       │ true          │
+└─────────┴───────────────┘
+┌─────────┬───────────┐
+│ (index) │ Values    │
+├─────────┼───────────┤
+│ 0       │ 'number'  │
+│ 1       │ 'string'  │
+│ 2       │ 'boolean' │
+└─────────┴───────────┘
+*/
---
+++ JavaScript_Basics/Variables.js
@@ -0,0 +1,10 @@
+const accountId = 1444 // Can't Be modified the constant Value
+let accoutEmail = "Sohaib@goole.com";
+var accoutPass = "Sohaib123";
+accountCity = "Azangarh";
+
+accoutEmail = "Azmi@gmail.com";
+accoutPass = "12345";
+accountCity = "MAu";
+
+console.table([accountId,accoutEmail,accoutPass,accountCity]);
